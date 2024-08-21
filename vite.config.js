c83808d9bbdd1213ea1a495ed3dd49a31e159d88
mateoUNC <mateo.rodriguez@mi.unc.edu.ar>
2024-08-21 20:27:21 -0300

    vit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'esnext',  // o 'chrome91' si prefieres usar un navegador específico
-    rollupOptions: {
-      external: [
-        'firebase/app',
-        'firebase/auth',
-        'firebase/database',
-        'firebase/firestore',
-        'firebase/storage',
-        'firebase/messaging',
-        'firebase/functions',
-        'firebase/analytics'
-      ]
+  resolve: {
+    alias: {
+      'firebase/auth': 'firebase/auth/dist/index.esm.js',
+      'firebase/app': 'firebase/app/dist/index.esm.js',
+      'firebase/database': 'firebase/database/dist/index.esm.js'
     }
   }
-})
+});
