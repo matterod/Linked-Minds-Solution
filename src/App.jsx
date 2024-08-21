70e32c0d3a171404ec9db87cfb2a2c8a4b62ba73
mateoUNC <mateo.rodriguez@mi.unc.edu.ar>
2024-08-21 20:46:50 -0300

    non

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import './App.css';
-import Globe from './components/Globe'; // Asegúrate de la ruta correcta
+import React, { useState } from 'react';
+import Auth from './components/Auth';
+import ControlPanel from './components/ControlPanel';
+import './App.css'; // Asegúrate de definir tus estilos aquí.
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [introVisible, setIntroVisible] = useState(true);
+
+  const handleStart = () => {
+    setIntroVisible(false); // Ocultar la introducción y mostrar la autenticación
+  };
+
   return (
     <div className="App">
-      <div className="content-container">
-        <h1>Linked Minds Solutions</h1>
-        <button>Iniciar sesión</button>
-      </div>
-      <Globe />
+      {introVisible ? (
+        <div className="intro-container">
+          <h1>Bienvenido a Linked Minds Solutions</h1>
+          <p>Nos especializamos en soluciones IoT innovadoras para conectar el mundo de manera eficiente.</p>
+          <button onClick={handleStart}>Comenzar</button>
+        </div>
+      ) : (
+        <div>
+          <Auth setUser={setUser} />
+          <ControlPanel user={user} />
+        </div>
+      )}
     </div>
   );
 }
