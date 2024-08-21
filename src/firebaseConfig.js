90f2d35b59e0766da0ab4f5800c611bb768f5fe6
mateoUNC <mateo.rodriguez@mi.unc.edu.ar>
2024-08-21 19:34:41 -0300

    commite

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,16 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANGLfDfRnsIfN3k-COWI22Y0bi8emK4Os",
-  authDomain: "esp32rinconada.firebaseapp.com",
-  databaseURL: "https://esp32rinconada-default-rtdb.firebaseio.com",
-  projectId: "esp32rinconada",
-  storageBucket: "esp32rinconada.appspot.com",
-  messagingSenderId: "82707406557",
-  appId: "1:82707406557:web:62f5993a30a39b7f130534",
-  measurementId: "G-84QEWN29ZH"
+    apiKey: "AIzaSyANGLfDfRnsIfN3k-COWI22Y0bi8emK4Os",
+    authDomain: "esp32rinconada.firebaseapp.com",
+    databaseURL: "https://esp32rinconada-default-rtdb.firebaseio.com",
+    projectId: "esp32rinconada",
+    storageBucket: "esp32rinconada.appspot.com",
+    messagingSenderId: "82707406557",
+    appId: "1:82707406557:web:62f5993a30a39b7f130534",
+    measurementId: "G-84QEWN29ZH"
 };
 
 const app = initializeApp(firebaseConfig);
