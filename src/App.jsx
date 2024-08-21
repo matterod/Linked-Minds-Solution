0e6e509c432cf952bc31905e90ab23ef53ff8146
mateoUNC <mateo.rodriguez@mi.unc.edu.ar>
2024-08-21 19:19:02 -0300

    Actualizacion pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Auth from './components/Auth';
 import ControlPanel from './components/ControlPanel';
+import './App.css';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -8,8 +9,9 @@ function App() {
   return (
     <div>
       {!user ? (
-        <div id="login-container">
-          <h1>Inicia sesión</h1>
+        <div id="welcome-container">
+          <h1>Bienvenido a World Things Control</h1>
+          <p>Con esta página, puedes controlar tus dispositivos IoT de forma remota y segura. Por favor, inicia sesión con Google para continuar.</p>
           <Auth setUser={setUser} />
         </div>
       ) : (
