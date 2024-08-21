256afabb87d281fcbfec9ef45393f7150ac85fe7
mateoUNC <mateo.rodriguez@mi.unc.edu.ar>
2024-08-21 20:29:47 -0300

    hola

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'firebase/auth': 'firebase/auth/dist/index.esm.js',
-      'firebase/app': 'firebase/app/dist/index.esm.js',
-      'firebase/database': 'firebase/database/dist/index.esm.js'
-    }
-  }
 });
